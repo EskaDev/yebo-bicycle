ba55c730cc4d887324fd94533327872329fceab4
EskaDev <olegpavlivwebdev@gmail.com>
2020-05-07 09:38:20 +0200

    end brands section  and add slider section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 
 
+
+
 // ibg
 function ibg() {
 
@@ -22,3 +24,22 @@ $(document).ready(function () {
 		$('body').toggleClass('lock');
 	});
 });
+//slides
+if ($('.slider__body').length > 0) {
+	$('.slider__body').slick({
+		//autoplay: true;
+		//infinite: false;
+		dots: true,
+		arrow: false,
+		accessibility: false,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		autoplaySpeed: 3000,
+		nextArrow: '<button type="button" class="slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-next"></button>',
+		responsive: [{
+			breakpoint: 768,
+			settings: {}
+		}]
+	});
+}
